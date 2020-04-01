ec35233c7a0bfb27c78bce8bb051b1a9c0f9c0d3
Chuljin Kim <jin2opa@gmail.com>
2020-04-02 04:50:47 +0900

    version2

---
+++ Local-sqlDB03.sql
@@ -0,0 +1,17 @@
+CREATE SEQUENCE idSEQ;
+INSERT INTO buytbl VALUES(idSEQ.NEXTVAL, 'KBS', '운동화', NULL, 30, 2);
+INSERT INTO buytbl VALUES(idSEQ.NEXTVAL, 'KBS', '노트북', '전자', 1000, 1);
+INSERT INTO buytbl VALUES(idSEQ.NEXTVAL, 'JYP', '모니터', '전자', 200, 1);
+INSERT INTO buytbl VALUES(idSEQ.NEXTVAL, 'BBK', '모니터', '전자', 200, 5);
+INSERT INTO buytbl VALUES(idSEQ.NEXTVAL, 'KBS', '청바지', '의류', 50, 3);
+INSERT INTO buytbl VALUES(idSEQ.NEXTVAL, 'BBK', '메모리', '전자', 80, 10);
+INSERT INTO buytbl VALUES(idSEQ.NEXTVAL, 'SSK', '책', '서적', 15, 5);
+INSERT INTO buytbl VALUES(idSEQ.NEXTVAL, 'EJW', '책', '서적', 15, 2);
+INSERT INTO buytbl VALUES(idSEQ.NEXTVAL, 'EJW', '청바지', '의류', 50, 1);
+INSERT INTO buytbl VALUES(idSEQ.NEXTVAL, 'BBK', '운동화', NULL, 30, 2);
+INSERT INTO buytbl VALUES(idSEQ.NEXTVAL, 'EJW', '책', '서적', 15, 1);
+INSERT INTO buytbl VALUES(idSEQ.NEXTVAL, 'BBK', '운동화', NULL, 30, 2);
+
+COMMIT;
+
+
